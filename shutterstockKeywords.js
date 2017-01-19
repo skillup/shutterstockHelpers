cb07b5b81121409d5395092abca48f3a8de7cdf1
skillup <jakegfx@gmail.com>
2017-01-19 10:55:58 +0200

    This is where it all begins...

---
+++ shutterstockKeywords.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name		Shutterstock Keywords
+// @author		skillup (skillup.lj.ru)
+// @version		1.0
+// @description	Easy to copy image keywords.
+// @include		https://www.shutterstock.com/image-photo/*
+// @include		https://www.shutterstock.com/image-vector/*
+// ==/UserScript==
+
+(function() {
+    let keywords = document.getElementsByClassName("col-xs-12 text-left"),
+        myKeys = keywords[0].innerHTML.match(/\/search\/\w+/g).map(x => x.replace(/\/search\//, '')).join(' '),
+        node = document.createElement("p");
+    node.style.cssText = 'padding: 14px; margin-top: 48px; text-align: left;';
+    node.innerHTML = myKeys;
+    document.querySelector(".product-page-keywords").appendChild(node);
+})();
