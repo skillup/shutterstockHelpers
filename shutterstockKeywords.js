6216952589b4fec1cdebad533531321f898b41d9
skillup <jakegfx@gmail.com>
2017-01-22 19:35:59 +0200

    Refactored and added support for longer phrases.

--- shutterstockKeywords.js
+++ shutterstockKeywords.js
@@ -10,7 +10,7 @@
 
 (() => {
     let node = document.createElement("p");
-    node.innerHTML = document.getElementsByClassName("col-xs-12 text-left")[0].innerHTML.match(/h\/\w+((-|\+)\w+)?/g).map(x => x.slice(2).replace('+', ' ')).join(', ');
+    node.innerHTML = document.querySelector(".col-xs-12").innerHTML.match(/h\/\w+((-|\+)\w+(\+\w+)?)?/g).map(x => x.slice(2).replace(/\+/g, ' ')).join(', ');
     node.style.cssText = 'padding: 14px; margin-top: 48px; text-align: left;';
     document.querySelector(".product-page-keywords").appendChild(node);
 })();
