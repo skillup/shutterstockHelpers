a84c919396392e1ea78312ffdb28c39a192ccc22
skillup <jakegfx@gmail.com>
2017-02-08 18:28:11 +0200

    Release v1.1

--- shutterstockKeywords.js
+++ shutterstockKeywords.js
@@ -1,7 +1,8 @@
 // ==UserScript==
 // @name          Shutterstock Keywords
 // @author        skillup (skillup.lj.ru)
-// @version       1.0
+// @source        http://skillup.lj.ru
+// @version       1.1
 // @description   Easy to copy image keywords.
 // @include       https://www.shutterstock.com/image-photo/*
 // @include       https://www.shutterstock.com/image-vector/*
@@ -9,8 +10,17 @@
 // ==/UserScript==
 
 (() => {
-    let node = document.createElement("p");
-    node.innerHTML = document.querySelector(".col-xs-12").innerHTML.match(/h\/\w+((-|\+)\w+(\+\w+)?)?/g).map(x => x.slice(2).replace(/\+/g, ' ')).join(', ');
-    node.style.cssText = 'padding: 14px; margin-top: 48px; text-align: left;';
-    document.querySelector(".product-page-keywords").appendChild(node);
+    let newKeywords = document.createElement("p"),
+        keywordsCount = document.createElement("span"),
+        keys = document.querySelector(".product-page-keywords"),
+        parent = keys.parentNode;
+    newKeywords.innerHTML = document.querySelector(".col-xs-12").innerHTML.match(/h\/\w+((-|\+)\w+(\+\w+)?)?/g).map(x => x.slice(2).replace(/\+/g, ' ')).join(', ');
+    let html = newKeywords.innerHTML.split(', ');
+    html = html.map(a => a = `<a href="/search/${a}">${a}</a>`);
+    keywordsCount.innerHTML = `${html.length} keywords:<br>`;
+    newKeywords.innerHTML = keywordsCount.innerHTML;
+    newKeywords.innerHTML += html.join(', ');
+    newKeywords.style.cssText = 'padding-left: 14px; text-align: left; line-height: 150%';
+    parent.appendChild(newKeywords);
+    parent.removeChild(keys);
 })();
