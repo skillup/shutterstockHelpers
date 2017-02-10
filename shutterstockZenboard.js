ce5fb804dca5967baa479050afdeb600309ce6ea
skillup <jakegfx@gmail.com>
2017-02-10 18:22:03 +0200

    Footer returned.

--- shutterstockZenboard.js
+++ shutterstockZenboard.js
@@ -17,7 +17,7 @@
         for (var i = this.length - 1; i >= 0; i--)
             if (this[i] && this[i].parentElement) this[i].parentElement.removeChild(this[i]);
     };
-    for (let i = 8; i < temp.length; i++)
+    for (let i = 8; i < temp.length - 4; i++)
         temp[i].remove();
     temp[7].parentNode.className = '';
     temp[7].parentNode.style.cssText = 'width: 400px; margin: 0 auto; margin-top: 200px;';
