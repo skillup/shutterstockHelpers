4e93b075e9e1a8904d0a6e2be93c4eae8040dbe9
vanstock <vanstock@ya.ru>
2019-06-19 15:58:31 +0300

    Сток сменил верстку
    Сток сменил верстку, я чуток поправил ходы к данным

--- shutterstockKeywords.js
+++ shutterstockKeywords.js
@@ -2,7 +2,7 @@
 // @name          Shutterstock Keywords
 // @author        skillup (skillup.lj.ru)
 // @source        http://skillup.lj.ru
-// @version       1.3
+// @version       1.4
 // @description   Easy to copy image keywords.
 // @include       https://www.shutterstock.com/image-photo/*
 // @include       https://www.shutterstock.com/image-vector/*
@@ -10,12 +10,12 @@
 // ==/UserScript==
 
 (() => {
-    let keywords = document.querySelectorAll('.js-product-keywords-holder')[0].children,
+    let keywords = document.querySelectorAll('.C_a_c')[0].children[0].children[0].children,
         newKeywords = document.createElement("p"),
-        keys = document.querySelector(".product-page-keywords"),
+        keys = document.querySelector(".C_a_c"),
         parent = keys.parentNode;
 
-    keywords = [...keywords].map(k => k.innerHTML).map(a => `<a href="/search/${a}">${a}</a>`);
+    keywords = [...keywords].map(k => k.innerText).map(a => `<a href="/search/${a}">${a}</a>`);
 
     newKeywords.innerHTML = `${keywords.length} keywords:<br>${keywords.join(', ')}`;
     newKeywords.style.cssText = 'padding-left: 14px; text-align: left; line-height: 150%';
