7c687f92af533fa714e9e41f285614c842ca7a2b
Oleg <jakegfx@gmail.com>
2020-03-07 23:54:52 +0200

    Update shutterstockKeywords.js

--- shutterstockKeywords.js
+++ shutterstockKeywords.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name          Shutterstock Keywords
-// @author        skillup (skillup.lj.ru)
+// @author        Oleg Skillup (skillup.lj.ru)
 // @source        http://skillup.lj.ru
-// @version       1.4
+// @version       1.5
 // @description   Easy to copy image keywords.
 // @include       https://www.shutterstock.com/image-photo/*
 // @include       https://www.shutterstock.com/image-vector/*
@@ -10,16 +10,24 @@
 // ==/UserScript==
 
 (() => {
-    let keywords = document.querySelectorAll('.C_a_c')[0].children[0].children[0].children,
-        newKeywords = document.createElement("p"),
-        keys = document.querySelector(".C_a_c"),
-        parent = keys.parentNode;
+    const $ = document.querySelectorAll.bind(document);
+    let keywords = $('.C_a_c')[0].children[0].children[0].children,
+        newKeywords = document.createElement("div"),
+        my_parent = $('body')[0];
 
     keywords = [...keywords].map(k => k.innerText).map(a => `<a href="/search/${a}">${a}</a>`);
 
     newKeywords.innerHTML = `${keywords.length} keywords:<br>${keywords.join(', ')}`;
-    newKeywords.style.cssText = 'padding-left: 14px; text-align: left; line-height: 150%';
+    newKeywords.style.cssText = `
+        text-align: left;
+        width: 450px;
+        background-color: white;
+        line-height: 150%;
+        position: absolute;
+        padding: 20px;
+        right: 10px;
+        top: 150px;
+        z-index: 9999999;`;
+    my_parent.appendChild(newKeywords);
 
-    parent.appendChild(newKeywords);
-    parent.removeChild(keys);
 })();
